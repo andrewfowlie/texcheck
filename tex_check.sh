41b1157c64126ff2a23c7f15221b393a8afc8cde
andrew-laptop <andrew.fowlie@kbfi.ee>
2015-11-12 15:11:07 +1100

    Fixed a few bugs, tidied

--- tex_check.sh
+++ tex_check.sh
@@ -1,39 +1,82 @@
 #!/bin/bash
-# Spell check a LaTeX file.
 
-if [ -z "$1" ]
+#
+# Check a LaTeX build for bugs, including spellings and common typos
+# Usage ./tex_check.sh FILE_NAME
+#
+
+TEX=$1
+BASE=$(echo $TEX | cut -f 1 -d '.')
+BIBLOG=$BASE.blg
+
+if ! [ -e $TEX ]
   then
-    echo "No file name supplied."
+    echo "# File does not exist"
     exit
 fi
 
-# Check spelling with aspell.
-# English dictionary.
-# p (o) - Skip (optional) argument.
-# P (O) - Check (optional) arguemnt.
-# http://aspell.net/0.50-doc/man-html/4_Customizing.html
-echo -e "\e[00;31m[1]\e[00m Checking spelling."
+#
+# Check spelling with aspell - see http://aspell.net/0.50-doc/man-html/4_Customizing.html
+#
+
+echo "# Checking spelling"
+
 aspell \
+--home-dir `pwd` \
+--sug-mode normal \
 --mode=tex \
 --master=en_GB-ise \
---home-dir='./'\
---add-tex-command='todo p' \
 --add-tex-command='bibliographystyle p' \
 --add-tex-command='bibliography p' \
-check $1 # Finally check!              
+--add-tex-command='cite p' \
+--add-tex-command='ref p' \
+--add-tex-command='label p' \
+check $TEX # Finally check!              
 
-# Check particular typos.
-echo -e "\e[00;31m[2]\e[00m Checking typos."
-# Missing space in new sentence.
-awk '{for(i=1;i<=NF;i++){print $i}}' $1 | grep -i "\.[a-z]" $1 | grep -TEn --color -v '\.pdf' 
-# Missing space after bracket.
-awk '{for(i=1;i<=NF;i++){print $i}}' $1 | grep -TEni --color  "\)[a-z]\||}[a-z]" $1
-# Missing capital in new sentence.
-awk '{for(i=1;i<=NF;i++){print $i}}' $1 | grep -TEn --color  "\. [a-z]" $1 
+#
+# Check for missing bibliography items
+#
 
-# Check for repeated words.
-echo -e "\e[00;31m[3]\e[00m Checking repeated words."
-# On a single line.
-grep -TEin --color  "\b(\w+)\b\s*\1\b" $1
-# Across a linebreak.
-awk '{printf("%s\n   %.5d - %.5d: ... %s ",$0, FNR,FNR+1, $NF)}' $1 | grep -TEi --color  "\.\.\. \b(\w+)\b\s*\1\b"        
+if ! [ -e "$BIBLOG" ]
+  then
+    echo "# No BibTeX log"
+  else
+    echo "# Warnings from BibTeX"
+    grep --color 'Warning' $BIBLOG 
+fi
+
+#
+# Check for typos
+#
+
+
+#
+# Missing space in new sentence
+#
+
+echo "# Missing space in new sentence"
+awk '{for(i=1;i<=NF;i++){print $i}}' $TEX | grep -i "\.[a-z]" $TEX | grep -TEn --color -v '\.pdf' 
+
+#
+# Missing space before or after bracket
+#
+
+echo "# Missing space after bracket"
+awk '{for(i=1;i<=NF;i++){print $i}}' $TEX | grep -TEni --color  "\)[a-z]" $TEX
+echo "# Missing space before bracket"
+awk '{for(i=1;i<=NF;i++){print $i}}' $TEX | grep -TEni --color  "[a-z]\(" $TEX
+
+#
+# Missing capital in new sentence
+#
+
+echo "# Missing capital in new sentence"
+awk '{for(i=1;i<=NF;i++){print $i}}' $TEX | grep -TEn --color  "\. [a-z]" $TEX 
+
+#
+# Repeated words
+#
+
+echo "# Repeated words"
+grep -TEin --color  "\b(\w+)\b\s*\1\b" $TEX
+awk '{printf("%s\n   %.5d - %.5d: ... %s ",$0, FNR,FNR+1, $NF)}' $TEX | grep -TEi --color "\.\.\. \b(\w+)\b\s*\1\b"        
